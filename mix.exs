34cf3c2ef068c9dbd26dd16eb9c89a6fbc87b8a1
RaymondLoranger <Raymond.Loranger@bell.net>
2018-03-31 16:18:49 -0400

    0.1.0

--- mix.exs
+++ mix.exs
@@ -7,8 +7,31 @@ defmodule Islands.Engine.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      name: "Islands Engine",
+      source_url: source_url(),
+      description: description(),
+      package: package(),
       deps: deps(),
       dialyzer: [plt_add_apps: [:io_ansi_table]]
+    ]
+  end
+
+  defp source_url do
+    "https://github.com/RaymondLoranger/islands_engine"
+  end
+
+  defp description do
+    """
+    Models an Islands game.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "config/persist*.exs"],
+      maintainers: ["Raymond Loranger"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => source_url()}
     ]
   end
 
