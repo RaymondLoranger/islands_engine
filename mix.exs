a3219b28568c3deedc23539483f2158fcf1acaee
RaymondLoranger <Raymond.Loranger@bell.net>
2018-10-28 21:08:37 -0400

    0.1.26

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Engine.Mixfile do
   def project do
     [
       app: :islands_engine,
-      version: "0.1.25",
+      version: "0.1.26",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       name: "Islands Engine",
