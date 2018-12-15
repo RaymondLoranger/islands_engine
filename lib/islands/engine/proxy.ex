ec6491a9e69c88d87e746c554c03fc96b0916f47
RaymondLoranger <Raymond.Loranger@bell.net>
2018-12-15 15:12:20 -0500

    0.1.63

--- lib/islands/engine/proxy.ex
+++ lib/islands/engine/proxy.ex
@@ -5,7 +5,6 @@ defmodule Islands.Engine.Proxy do
   """
 
   alias __MODULE__.{Error, GameNotStarted, Info}
-  alias IO.ANSI.Plus, as: ANSI
   alias Islands.Engine.Game.{Server, Tally}
   alias Islands.Engine
 
@@ -18,7 +17,15 @@ defmodule Islands.Engine.Proxy do
   catch
     :exit, reason ->
       Error.log(:exit, reason)
-      wait_and_call(request, game_name, reason)
+
+      case wait(game_name, reason, @times) do
+        {:registered, game_name} ->
+          game_name |> Server.via() |> GenServer.call(request)
+
+        {:unregistered, game_name} ->
+          Info.log(:game_not_started, game_name)
+          game_name |> GameNotStarted.message() |> IO.puts()
+      end
   end
 
   @spec stop(atom, String.t()) :: :ok
@@ -27,35 +34,22 @@ defmodule Islands.Engine.Proxy do
   catch
     :exit, exit_reason ->
       Error.log(:exit, exit_reason)
-      wait_and_stop(reason, game_name, exit_reason)
+
+      case wait(game_name, reason, @times) do
+        {:registered, game_name} ->
+          game_name |> Server.via() |> GenServer.stop(reason)
+
+        {:unregistered, game_name} ->
+          Info.log(:game_not_started, game_name)
+          game_name |> GameNotStarted.message() |> IO.puts()
+      end
   end
 
   ## Private functions
 
-  @spec wait_and_call(tuple, String.t(), term) :: Tally.t() | :ok
-  defp wait_and_call(request, game_name, reason) do
-    game_name |> wait(reason, @times) |> Server.via() |> GenServer.call(request)
-  catch
-    :exit, reason ->
-      Error.log(:exit, reason)
-      game_name |> GameNotStarted.message() |> ANSI.puts()
-  end
-
-  @spec wait_and_stop(atom, String.t(), term) :: :ok
-  defp wait_and_stop(reason, game_name, exit_reason) do
-    game_name
-    |> wait(exit_reason, @times)
-    |> Server.via()
-    |> GenServer.stop(reason)
-  catch
-    :exit, exit_reason ->
-      Error.log(:exit, exit_reason)
-      game_name |> GameNotStarted.message() |> ANSI.puts()
-  end
-
   # On restarts, wait if name not yet registered...
-  @spec wait(String.t(), term, non_neg_integer) :: String.t()
-  defp wait(game_name, _reason, 0), do: game_name
+  @spec wait(String.t(), term, non_neg_integer) :: {atom, String.t()}
+  defp wait(game_name, _reason, 0), do: {:unregistered, game_name}
 
   defp wait(game_name, reason, times_left) do
     Info.log(:game_not_registered, game_name, @timeout, times_left, reason)
@@ -64,7 +58,7 @@ defmodule Islands.Engine.Proxy do
     case Engine.game_pid(game_name) do
       pid when is_pid(pid) ->
         Info.log(:game_registered, game_name, pid, times_left, reason)
-        game_name
+        {:registered, game_name}
 
       nil ->
         wait(game_name, reason, times_left - 1)
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Islands.Engine.Mixfile do
   def project do
     [
       app: :islands_engine,
-      version: "0.1.62",
+      version: "0.1.63",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       name: "Islands Engine",
