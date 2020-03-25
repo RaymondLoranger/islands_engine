7204e8cf29b1481f451c101aec88e1b5d258a6da
RaymondLoranger <Raymond.Loranger@bell.net>
2020-03-24 21:12:46 -0400

    0.2.31

--- lib/islands/engine/guards.ex
+++ lib/islands/engine/guards.ex
@@ -3,7 +3,6 @@ defmodule Islands.Engine.Guards do
   Defines guards for the Islands Engine API.
   """
 
-  @coord_range 1..10
   @genders [:f, :m]
   @island_types [:atoll, :dot, :l_shape, :s_shape, :square]
   @player_ids [:player1, :player2]
@@ -21,10 +20,10 @@ defmodule Islands.Engine.Guards do
 
   defguard valid_island_args(game_name, player_id, island_type, row, col)
            when is_binary(game_name) and player_id in @player_ids and
-                  island_type in @island_types and row in @coord_range and
-                  col in @coord_range
+                  island_type in @island_types and is_integer(row) and
+                  is_integer(col)
 
   defguard valid_coord_args(game_name, player_id, row, col)
            when is_binary(game_name) and player_id in @player_ids and
-                  row in @coord_range and col in @coord_range
+                  is_integer(row) and is_integer(col)
 end
