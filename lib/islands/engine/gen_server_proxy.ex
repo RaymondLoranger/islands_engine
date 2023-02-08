ecda51dd84891f00bc6aa90be2d19e0cd914b334
RaymondLoranger <Raymond.Loranger@bell.net>
2023-02-07 19:01:06 -0500

    0.2.59

--- lib/islands/engine/gen_server_proxy.ex
+++ lib/islands/engine/gen_server_proxy.ex
@@ -5,11 +5,11 @@ defmodule Islands.Engine.GenServerProxy do
   alias Islands.Engine.GameServer
   alias Islands.Game
 
-  @impl GenServer.Proxy
+  # @impl GenServer.Proxy
   @spec server_name(Game.name()) :: GenServer.name()
   defdelegate server_name(game_name), to: GameServer, as: :via
 
-  @impl GenServer.Proxy
+  # @impl GenServer.Proxy
   @spec server_unregistered(Game.name()) :: :ok
   def server_unregistered(game_name) do
     ANSI.puts([
